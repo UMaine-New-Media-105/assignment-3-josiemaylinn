8fa512773729136b53892bd1ec77d304675b8aa1
josiemaylinn <124173145+josiemaylinn@users.noreply.github.com>
2023-03-19 16:00:03 -0400

    Question 1

---
+++ main.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(50, 50);
+}
+
+function draw() {
+  noStroke();
+  fill("brown");
+  ellipse(25, 35, 25);
+  fill("white");
+  ellipse(25, 15, 25);
+  fill("black");
+  ellipse(15, 15, 10);
+  ellipse(35, 15, 10);
+  fill("orange");
+  ellipse(15, 45, 10);
+  ellipse(35, 45, 10);
+}
