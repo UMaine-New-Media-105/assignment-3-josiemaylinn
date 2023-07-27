f11e200ff9a44cd03b580b8a8466b869968a07b0
josiemaylinn <124173145+josiemaylinn@users.noreply.github.com>
2023-07-27 15:48:40 -0400

    Create question4.js

---
+++ question4.js
@@ -0,0 +1,51 @@
+function setup() {
+  createCanvas(400, 400);
+  seedX = random(400);
+  seedY = 0;
+  
+
+}
+
+function draw() {
+  background("lightblue");
+  addCatcher(mouseX, 350);
+  dropSeed()
+}
+
+function addCatcher(x, y) {
+  push();
+  translate(x, y);
+  
+  noStroke();
+  fill("brown");
+  ellipse(25, 35, 25);
+  fill("white");
+  ellipse(25, 15, 25);
+  fill("black");
+  ellipse(15, 15, 10);
+  ellipse(35, 15, 10);
+
+  if (mouseIsPressed) {
+    fill("orange");
+    ellipse(15, 5, 10);
+    ellipse(35, 5, 10);
+  } else {
+    fill("orange");
+    ellipse(15, 45, 10);
+    ellipse(35, 45, 10);
+  }
+  
+  pop();
+}
+
+function dropSeed() {
+
+  if (mouseButton) {
+  noStroke;
+  seedY = seedY + 5;
+  fill("crimson");
+  circle(seedX, seedY, 10);
+  }
+
+
+}
