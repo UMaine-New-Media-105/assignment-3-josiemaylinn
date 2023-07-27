3ffb2d8b9d41a598e3ef318e1a65e6b0e97fcac7
josiemaylinn <124173145+josiemaylinn@users.noreply.github.com>
2023-07-27 16:45:25 -0400

    Question 5

--- main.js
+++ main.js
@@ -2,22 +2,28 @@ function setup() {
   createCanvas(400, 400);
   seedX = random(400);
   seedY = 0;
-  
-
+  catcherBody = ["brown"];
+  let overlap;
 }
 
 function draw() {
-  background("lightblue");
   addCatcher(mouseX, 350);
-  dropSeed()
+  dropSeed();
 }
 
 function addCatcher(x, y) {
   push();
+
+  if (seedY >= 350 && seedX >= x - 30 && seedX <= x + 30) {
+    catcherBody = "green";
+  } else {
+    catcherBody = "brown";
+  }
+
   translate(x, y);
-  
+  background("lightblue");
   noStroke();
-  fill("brown");
+  fill(catcherBody);
   ellipse(25, 35, 25);
   fill("white");
   ellipse(25, 15, 25);
@@ -25,27 +31,18 @@ function addCatcher(x, y) {
   ellipse(15, 15, 10);
   ellipse(35, 15, 10);
 
-  if (mouseIsPressed) {
-    fill("orange");
-    ellipse(15, 5, 10);
-    ellipse(35, 5, 10);
-  } else {
-    fill("orange");
-    ellipse(15, 45, 10);
-    ellipse(35, 45, 10);
-  }
-  
+  fill("orange");
+  ellipse(15, 45, 10);
+  ellipse(35, 45, 10);
+
   pop();
 }
 
 function dropSeed() {
-
   if (mouseButton) {
-  noStroke;
-  seedY = seedY + 5;
-  fill("crimson");
-  circle(seedX, seedY, 10);
+    noStroke;
+    seedY = seedY + 5;
+    fill("crimson");
+    circle(seedX, seedY, 10);
   }
-
-
 }
